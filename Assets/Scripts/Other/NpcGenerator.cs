69ebeb7d281642d2acf5c6928a7290b7a5600bbe
tsubasamusu <rdr2daisuki@gmail.com>
2022-12-22 12:13:52 +0900

    NpcGeneratorにnpcの生成位置のリストを作成する処理を追加。

--- Assets/Scripts/Other/NpcGenerator.cs
+++ Assets/Scripts/Other/NpcGenerator.cs
@@ -17,16 +17,6 @@ namespace CallOfUnity
             //生成位置のリストを取得
             List<Vector3> spawnPosList = GetSpawnPosList();
 
-            //取得したリストの要素数が正しくなかったら
-            if (spawnPosList.Count != ConstData.TEAMMATE_NUMBER * 2 - 1)
-            {
-                //問題を報告
-                Debug.Log("適切な数の生成位置を作成してください");
-
-                //以降の処理を行わない
-                return;
-            }
-
             //生成位置のリストの要素数だけ繰り返す
             for (int i = 0; i < spawnPosList.Count; i++)
             {
@@ -53,10 +43,34 @@ namespace CallOfUnity
         /// <returns>生成位置のリスト</returns>
         private List<Vector3> GetSpawnPosList()
         {
-            //（仮）
-            return new List<Vector3> { Vector3.zero };
+            //リストを用意する
+            List<Vector3> spawnPosList = new();
 
-            //TODO:生成位置のリストを取得する処理
+            //必要なNPCの数だけ繰り返す
+            for (int i = 0; i < ConstData.TEAMMATE_NUMBER * 2 - 1; i++)
+            {
+                //チーム0の生成位置を作成しているなら
+                if (i <= ConstData.TEAMMATE_NUMBER - 2)
+                {
+                    //最初の生成位置のx座標を取得
+                    float firstPosX = -2f*((ConstData.TEAMMATE_NUMBER-1) / 2f);
+
+                    //作成した座標をリストに追加する
+                    spawnPosList.Add(new Vector3(firstPosX+(2f*i),0f,-25f));
+
+                    //次の繰り返し処理に移る
+                    continue;
+                }
+
+                //最初の生成位置のx座標を取得
+                float firstPosX2 = -2f*(ConstData.TEAMMATE_NUMBER / 2f);
+
+                //作成した座標をリストに追加する
+                spawnPosList.Add(new Vector3(firstPosX2 + (2f * (i- (ConstData.TEAMMATE_NUMBER-1))), 0f, 25f));
+            }
+
+            //作成したリストを返す
+            return spawnPosList;
         }
     }
 }
